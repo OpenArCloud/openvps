10a6cff06b5271ad332b23638de5e4d922ec0de9
gabor.soros <gabor.soros@nokia-bell-labs.com>
2025-10-03 13:05:47 +0200

    fix swapped parameters in zip_compress.py

--- mapbuilder/scripts_mapping/zip_compress.py
+++ mapbuilder/scripts_mapping/zip_compress.py
@@ -15,9 +15,9 @@ from pathlib import Path
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compressing a folder")
     parser.add_argument("--input_dir", type=str, default=None,
-                        help="Input zip file", required=True)
+                        help="Input directory", required=True)
     parser.add_argument("--zip_path", type=str, default=None,
-                        help="Output directory file", required=True)
+                        help="Output file", required=True)
     args = parser.parse_args()
 
     input_dir = Path(args.input_dir)
@@ -28,9 +28,11 @@ if __name__ == "__main__":
     try:
         zip_path = Path(args.zip_path)
         os.makedirs(zip_path.parent, exist_ok=True)
+
         if zip_path.suffix == '.zip':
-            zip_path.with_suffix('') # remove extension because make_archive will add it to the name
-        shutil.make_archive(str(input_dir), "zip", str(zip_path))
+            zip_path_nosuffix = zip_path.with_suffix('') # remove extension because make_archive will add it to the name
+        shutil.make_archive(base_name=str(zip_path_nosuffix), format="zip", root_dir=str(input_dir))
+        print("Successfully created zip file: " + str(zip_path))
     except Exception as ex:
         print("Exception occurred: " + str(ex))
         exit(-1)
